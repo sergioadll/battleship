257391536ae60f597c8f18e715a7318b84683cf2
sergioadll <sergio.adll@hotmail.com>
2020-12-23 23:36:36 +0000

    board completed

--- src/main.js
+++ src/main.js
@@ -2,9 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let gameBoard = [
+  [1, 1, 1, 1, 1, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 1, 1, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [1, 1, 1, 1, 0, 0, 0, 0, 0, 0]
+];
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let row = document.querySelectorAll(".yAxis"); //traemos todas las filas (array) del tablero por su clase
+  for (let i = 0; i < row.length; i++) {
+    //recorremos todas las filas
+    for (let j = 0; j < gameBoard[0].length; j++) {
+      //recorremos cada celda del tablero/matriz "gameBoard"
+      let cell = document.createElement("div"); //creamos cada cell y añadimos la classe cell para css
+      cell.classList.add("cell");
+      cell.innerHTML = gameBoard[i][j];
+      cell.value = gameBoard[i][j]; //aqui asignamos a cada celda el valor correspondiente del tablero. Ejemplo: gameboard[2][3]=0  ->  fila 2, clolumna 3
+
+      cell.addEventListener("click", e => {
+        fire(e);
+      });
+      row[i].appendChild(cell);
+    }
+  }
 };
+
+function fire(e) {
+  console.log(e.target.value);
+}
